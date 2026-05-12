b77d8d083703366c2b070ed6f43a6b9c6db4a689
Bilal Rafique <rafiqueb087@gmail.com>
2026-05-12 05:09:32 +0500

    render deploy

--- summarizer/settings.py
+++ summarizer/settings.py
@@ -27,9 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = [
-    "Baal.pythonanywhere.com"
-]
 
 
 # Application definition
@@ -45,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'summarizer.urls'
